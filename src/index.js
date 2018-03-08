fee3c974125d51701ad41744ca6f2a2c8fdf5814
Leaf-WXC <523045770@qq.com>
2018-03-08 21:23:34 +0800

    wxc

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-
+import React, { Component } from 'react';
+class App extends Component {
+    render() {
+        return (
+          <div>  
+              <h1>hello world</h1> 
+          </div>
+        );
+      }
+}
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
