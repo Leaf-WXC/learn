59a289c913f533c5eab2b05f3e85291427d778ca
Leaf-WXC <523045770@qq.com>
2018-03-09 16:47:30 +0800

    hello who练习

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import ReactDOM from 'react-dom';
 import React, { Component } from 'react';
-class App extends Component {
+class Greeting extends Component {
     render() {
-        return (
-          <div>  
-              <h1>hello world!</h1> 
-          </div>
-        );
-      }
+      return (
+        <div>  
+            <h1>{`hello ${this.props.who}`}</h1> 
+        </div>
+      );
+    }
 }
+      const App=(props,context)=>{
+        return (
+          <div>
+            <Greeting who={'world'} />
+            <Greeting who={'react'} />
+          </div>
+        )
+      }    
 ReactDOM.render(<App />, document.getElementById('root'));
 
