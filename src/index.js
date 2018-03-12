f35b98e74cbe80f3ed2e30c836ce9452fe6f6998
Leaf-WXC <523045770@qq.com>
2018-03-12 20:01:33 +0800

    第三次作业

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
 import ReactDOM from 'react-dom';
 import React, { Component } from 'react';
 class Greeting extends Component {
-    render() {
-      return (
-        <div>  
-            <h1>{`hello ${this.props.who}`}</h1> 
-        </div>
-      );
-    }
-}
+  state={
+    who:'world'
+  }
+
+  switchName=()=>{
+    if (this.state.who==='world')
+      this.setState((prevState,props)=>({who:'react'}))
+    else
+      this.setState((prevState,props)=>({who:'world'}))
+  }
+
+  render() {
+    return (
+      <div>  
+          <h1>{`hello ${this.state.who}`}</h1> 
+          <button onClick={this.switchName}> {this.state.who==='world'?'react':'world'}</button>
+      </div>
+    );
+  }
+} 
+
       const App=(props,context)=>{
         return (
           <div>
